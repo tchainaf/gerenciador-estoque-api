6a5831c25a25f044f4fe94e7ef3db8dd689db15b
eloahnoguchi <eloahnoguchi@hotmail.com>
2020-04-05 19:37:34 -0300

    Eloah-api

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 var productService = require('../services/product.service');
 
-// routes
+// routes dois get do produto, id e all
+// put delete post, dois gets
 router.get('/:id', getProduct);
+router.getAll('/all', getAllProduct);
+router.delete('/:id', deleteProduct);
+router.post('/', registerProduct);
+//router.put('/:id', updateProduct);
 
 module.exports = router;
 
@@ -19,4 +24,57 @@ function getProduct(req, res) {
         .catch(function (err) {
             res.status(400).send(err);
         });
+}
+function getAllProduct(req, res) {
+    productService.getAll()
+        .then(function (product) {
+            if (product) {
+                res.send(product);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+function deleteProduct(req, res) {
+    productService.delete(req.params.id)
+        .then(function (product) {
+            if (product) {
+                res.send(product);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+function registerProduct(req, res) {
+    productService.create(req.body)
+        .then(function (product) {
+            if (product) {
+                res.send(product);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+//function updateProduct(req, res) {
+//    productService.update(req.params.id)
+//        .then(function (product) {
+            if (product) {
+                res.send(product);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+
 }
--- services/product.service.js
+++ services/product.service.js
@@ -7,17 +7,69 @@ db.bind('products');
 var service = {};
 
 service.getById = getById;
+service.getAll = getAll;
+service.create = create;
+//service.update = update;
+service.delete = _delete;
 
 module.exports = service;
 
-function getById(_id) {
+function getById(id) {
     var deferred = Q.defer();
 
-    db.products.findById(_id, function (err, product) {
+    db.products.findById(id, function (err, product) {
         if (err)
             deferred.reject(err.name + ': ' + err.message);
 
         deferred.resolve();
+    });
+
+    return deferred.promise;
+}
+function create(product) {
+    console.log(product);
+    var deferred = Q.defer();
+
+    db.products.insert(
+        product,
+        function (err) {
+            if (err) {
+                console.log(err);
+                deferred.reject(err.name + ': ' + err.message);
+            }
+
+            deferred.resolve();
+        });
+    return deferred.promise;
+}
+function _delete(id) {
+    var deferred = Q.defer();
+
+    db.products.remove(
+        { id: mongo.helper.toObjectID(id) },
+        function (err) {
+            if (err)  {
+            console.log(err);
+            deferred.reject(err.name + ': ' + err.message);
+            }
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
+function getAll() {
+    var deferred = Q.defer();
+
+    db.products.find({}).toArray(function (err, product) {
+        if (err) {
+            console.log(err);
+            deferred.reject(err.name + ': ' + err.message);
+        }
+        console.log(product);
+            // user not found
+            deferred.resolve(product);
+        
     });
 
     return deferred.promise;
